c4c1ed89f61e9482114c7f99db054ca52dbebbed
Mikdat ÇOLAK <mikdat.colak@gmail.com>
2017-07-14 10:10:23 +0300

    001 Hello World Test

---
+++ İntelijProjects/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("adas");
+        System.out.print("Hey Hey Ma Ma");
+
+    }
+
+}
